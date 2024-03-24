7d9c96df663a7c8693316a31aadfdd993733aff8
brian-ambani <brainambani1@gmail.com>
2024-03-25 01:46:12 +0300

    updated the newNote form

--- client/src/pages/newNote.js
+++ client/src/pages/newNote.js
@@ -1,13 +1,20 @@
 const NewNote = () => {
   return (
-    <div>
-      <div>Title</div>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quas
-        optio voluptas iusto, blanditiis ipsum, quod inventore architecto
-        dolorum eius velit quia voluptate nemo laborum.
-      </div>
-    </div>
+    <form className="w-3/4 mt-8 flex flex-col items-start justify-items-start">
+      <input
+        type="text"
+        placeholder="Title"
+        className="input input-bordered w-full max-w-xs "
+      />
+
+      <textarea
+        placeholder="Content..."
+        className="mt-4 textarea textarea-bordered sm:textarea-sm sm:max-w-sm md:textarea-md md:max-w-md lg:textarea-lg w-full lg:max-w-lg"
+        rows="6"
+      ></textarea>
+      <br />
+      <button className="btn btn-primary ">Save</button>
+    </form>
   );
 };
 export default NewNote;
