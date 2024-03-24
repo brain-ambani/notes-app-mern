c494e344293c6d907d0dc7801c5bf00af0248e36
brian-ambani <brainambani1@gmail.com>
2024-03-25 01:55:48 +0300

    updated note component

--- client/src/components/Note.js
+++ client/src/components/Note.js
@@ -4,9 +4,13 @@ const Note = () => {
       <div className="card-body">
         <h2 className="card-title">Card title!</h2>
         <p>If a dog chews shoes whose shoes does he choose?</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio maiores
+          voluptatum possimus?
+        </p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Update</button>
-          <button className="btn ">Delete</button>
+          <button className="btn btn-outline btn-primary">Update</button>
+          <button className="btn btn-outline btn-warning">Delete</button>
         </div>
       </div>
     </div>
