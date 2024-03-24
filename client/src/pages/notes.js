98dbf6019457ef54f201dbae7dd1c44ad2aa80d9
brian-ambani <brainambani1@gmail.com>
2024-03-25 01:46:31 +0300

    updated the notes component

--- client/src/pages/notes.js
+++ client/src/pages/notes.js
@@ -1,7 +1,12 @@
+import Note from "../components/Note";
+
 const Notes = () => {
   return (
     <div>
       <div>All notes</div>
+      <Note />
+      <Note />
+      <Note />
     </div>
   );
 };
