35396d76e468866626031a4f5a33d7d119fa7205
brian-ambani <brainambani1@gmail.com>
2024-03-25 22:09:59 +0300

    updated the note and notes ui

--- client/src/components/Note.js
+++ client/src/components/Note.js
@@ -1,12 +1,15 @@
 const Note = () => {
   return (
-    <div className="card w-96 bg-base-100 shadow-xl mt-4">
+    <div className="card w-3/4 bg-base-100 shadow-xl mt-4 ">
       <div className="card-body">
-        <h2 className="card-title">Card title!</h2>
+        <h2 className="card-title uppercase">Card title!</h2>
         <p>If a dog chews shoes whose shoes does he choose?</p>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio maiores
-          voluptatum possimus?
+          voluptatum possimus? Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Libero suscipit possimus facere veritatis nesciunt
+          dolor inventore consequatur vitae numquam! Cumque, minus maxime est
+          aspernatur exercitationem odio omnis saepe reiciendis obcaecati?
         </p>
         <div className="card-actions justify-end">
           <button className="btn btn-outline btn-primary">Update</button>
--- client/src/pages/notes.js
+++ client/src/pages/notes.js
@@ -2,8 +2,13 @@ import Note from "../components/Note";
 
 const Notes = () => {
   return (
-    <div>
-      <div>All notes</div>
+    <div className="flex flex-col items-center">
+      <div className="text-3xl uppercase text-center py-3 font-bold">
+        All notes
+      </div>
+      <Note />
+      <Note />
+      <Note />
       <Note />
       <Note />
       <Note />
