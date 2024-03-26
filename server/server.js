7181eef7750d1647043ac40881876f5f87eb9707
brian-ambani <brainambani1@gmail.com>
2024-03-26 23:15:26 +0300

    successfully connected server to DB

--- server/server.js
+++ server/server.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+const MONGODB = process.env.MONGODB_URI;
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+mongoose
+  .connect(MONGODB)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(
+        `Server is running on port ${PORT} and successfully connected to MongoDB`
+      );
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
