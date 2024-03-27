25be6fc2cf9f1445fad61f6d2da00a95e7f398f2
brian-ambani <brainambani1@gmail.com>
2024-03-28 00:10:11 +0300

    add generate token while logging in or registering an account

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/userModel");
+const generateToken = require("../utils/generateToken");
 
 // Function to register a user
 const registerUser = async (req, res) => {
@@ -10,7 +11,9 @@ const registerUser = async (req, res) => {
 
   try {
     const user = await User.create({ username, email, password });
-    res.status(201).json({ id: user._id, username, email });
+    res
+      .status(201)
+      .json({ id: user._id, username, email, token: generateToken(user._id) });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -27,6 +30,7 @@ const loginUser = async (req, res) => {
       _id: user._id,
       username: user.username,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401).json({ error: "Invalid email or password" });
