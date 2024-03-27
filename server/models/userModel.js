4ee210c5c1e08e793c01d07eee7948888a604ad2
brian-ambani <brainambani1@gmail.com>
2024-03-27 23:49:31 +0300

    add method to compare the password

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -32,4 +32,10 @@ userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
+// Method to match the password
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 module.exports = mongoose.model("User", userSchema);
